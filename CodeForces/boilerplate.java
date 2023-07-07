41c0665abb27d9486ee81cbd3ce8211ad540df2a
rayan <rayan@rayyan>
2023-07-07 22:12:53 +0530

    CF_1846 Submission

--- CodeForces/boilerplate.java
+++ CodeForces/boilerplate.java
@@ -3,7 +3,7 @@ package CodeForces;
 import java.util.*;
 import java.io.*;
 
-class boilerplate {
+public class boilerplate {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     }
