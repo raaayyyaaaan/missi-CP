55eac33b91aeef696784321fa37fc5792a27779a
rayan <rayan@rayyan>
2023-07-09 04:16:01 +0530

    CF_1846

--- CodeForces/CF_1846B.java
+++ CodeForces/CF_1846B.java
@@ -28,40 +28,37 @@ public class CF_1846B {
     	
     	for (int i = 0; i < arr.length; i++) {
     		if ((arr[i][0] == arr[i][1]) && (arr[i][1] == arr[i][2])) {
-    			if (arr[i][0] == '.') {
-    				continue;
-//    				System.out.println("DRAW"); flag = true;
-    			}
-    			else {
-    				System.out.println(arr[i][0]); flag = true;
-    			}
+    			if (arr[i][0] != '.') System.out.println(arr[i][0]);
+    			flag = true;
+    			break;
     		}
     	}
+    	
+
     	for (int i = 0; i < arr[0].length; i++) {
     		if ((arr[0][i] == arr[1][i]) && (arr[1][i] == arr[2][i])) {
-    			if (arr[0][i] == '.') {
-    				continue;
-//    				System.out.println("DRAW"); flag = true;
-    			}
-    			else {
-    				System.out.println(arr[0][i]); flag = true;
+
+    			if (arr[0][i] != '.') {
+    				System.out.println(arr[0][i]); 
+    				flag = true;
+    				break;
     			}
     		}
     	}
+    	
+    	
     	if ((arr[0][0] == arr[1][1]) && (arr[1][1] == arr[2][2])) {
-			if (arr[0][0] == '.') {
-//				System.out.println("DRAW"); flag = true;
-			}
-			else {
-				System.out.println(arr[0][0]); flag = true;
+			if (arr[0][0] != '.') {
+				System.out.println(arr[0][0]);
+				flag = true;
+				
 			}
 		}
+    	
     	if ((arr[0][2] == arr[1][1]) && (arr[1][1] == arr[2][0])) {
-			if (arr[0][2] == '.') {
-//				System.out.println("DRAW"); flag = true;
-			}
-			else {
-				System.out.println(arr[0][2]); flag = true;
+			if (arr[0][2] != '.') {
+				System.out.println(arr[0][2]);
+				flag = true;
 			}
 		}
     	
