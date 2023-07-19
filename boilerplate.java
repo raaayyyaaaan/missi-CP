52b6017265ba45b54ce2375cdd7bdbbae82fa640
rayan <rayan@rayyan>
2023-07-20 03:39:15 +0530

    START99

--- boilerplate.java
+++ boilerplate.java
@@ -1,10 +1,12 @@
-package CodeForces;
-
 import java.util.*;
 import java.io.*;
 
 public class boilerplate {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
+    	int t = sc.nextInt();
+    	while (t-- > 0) {
+    		
+    	}
     }
 }
