46591f05e2f6e7492675254beb1a4f0c6b38993f
Rayyan Aziz <rayanaleem@gmail.com>
2024-04-09 09:42:24 +0530

    ++

---
+++ src/CodeForces/boilerplate.java
@@ -0,0 +1,14 @@
+package CodeForces;
+
+import java.util.*;
+
+public class boilerplate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+
+        }
+
+    }
+}
